470eb40c6a625a6b4f6fdb79e93f462d8a2a1b17
AzeriCui <494824778@qq.com>
2018-10-05 13:49:23 +0800

    修改Create验证过滤规则

--- Trunk/YaSuo.AdminLTE/Scripts/jquery.validate.extend.js
+++ Trunk/YaSuo.AdminLTE/Scripts/jquery.validate.extend.js
@@ -1,20 +1,65 @@
-﻿
-$.validator.setDefaults({
-    ignore: ".ignore, .select2-search__field"
-    , errorElement: "em"
-    , onfocusin: function (element) { $(element).valid(); }
-    , onfocusout: function (element) { $(element).valid(); }
-    , errorPlacement: function (error, element) {
+﻿(function ($, window, undefined) {
+    var Selector = {
+        IgnoreElement: ".ignore, .select2-search__field"
+        , ErrorBox: "em"
+        , ParentElement: ".form-group"
+        , SuccessElement: "has-success"
+        , ErrorElement: "has-error"
+        , HelpElement: ".help-block i"
+        , TipElement: "fa-bell-o"
+        , TipSuccessElement: "fa-check"
+        , TipErrorElement: "fa-close"
+    };
+
+    function Highlight(element, errorClass, validClass) {
+        $(element)
+            .parents(Selector.ParentElement)
+                .removeClass(Selector.SuccessElement)
+                .addClass(Selector.ErrorElement)
+            .find(Selector.HelpElement)
+                .removeClass(Selector.TipElement)
+                .removeClass(Selector.TipSuccessElement)
+                .addClass(Selector.TipErrorElement);
+    }
+
+    function Unhighlight(element, errorClass, validClass) {
+        $(element)
+            .parents(Selector.ParentElement)
+                .removeClass(Selector.ErrorElement)
+                .addClass(Selector.SuccessElement)
+            .find(Selector.HelpElement)
+                .removeClass(Selector.TipElement)
+                .removeClass(Selector.TipErrorElement)
+                .addClass(Selector.TipSuccessElement);
+    }
+
+    function OnFocusin(element) {
+        $(element).valid();
+    }
+
+    function OnFocusout(element) {
+        $(element).valid();
+    }
+
+    function ErrorPlacement(error, element) {
 
     }
-    , highlight: function (element, errorClass, validClass) {
-        $(element).parents(".form-group").removeClass("has-success").addClass("has-error")
-            .find(".help-block i").removeClass("fa-bell-o").removeClass("fa-check").addClass("fa-times-circle-o");
-    }
-    , unhighlight: function (element, errorClass, validClass) {
-        $(element).parents(".form-group").removeClass("has-error").addClass("has-success")
-            .find(".help-block i").removeClass("fa-bell-o").removeClass("fa-times-circle-o").addClass("fa-check");
-    }
-});
 
+    $.validator.setDefaults({
+        ignore: Selector.IgnoreElement
+        , errorElement: Selector.ErrorBox
+        , onfocusin: OnFocusin
+        , onfocusout: OnFocusout
+        , errorPlacement: ErrorPlacement
+        , highlight: Highlight
+        , unhighlight: Unhighlight
+    })
 
+    $(function () {
+        jQuery.validator.addMethod("Test2", function (value, element, params) {
+
+            return this.optional(element) || value == params;
+        }, "错误提示消息");
+    })
+
+}(jQuery, window))
